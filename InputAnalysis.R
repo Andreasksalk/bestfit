f9ad1227b93dac4a3754d65db77e76cc4c23c6dd
andreasksalk <andreassalk@HUAWEI_P10-1559581eb2c7e8.localdomain>
2019-06-06 11:33:21 +0200

    Last changes before release

--- InputAnalysis.R
+++ InputAnalysis.R
@@ -1,17 +1,30 @@
 # This code is created to fit basic distributions
 # Authors: Andreas K. Salk & Frederik Toftegaard
 
+# Function to Install and Load R Packages
+inst_load <- function(Required_Packages){
+  Remaining_Packages <- Required_Packages[!(Required_Packages %in% installed.packages()[,"Package"])];
+  
+  if(length(Remaining_Packages)) 
+  {
+    install.packages(Remaining_Packages);
+  }
+  for(package_name in Required_Packages)
+  {
+    library(package_name,character.only=TRUE,quietly=TRUE);
+  }
+}
+
+# The Packages needed to run the code 
+Required_Packages=c("openxlsx", "fBasics", "MASS", "data.table", "triangle", "shinydashboard", "shiny", "rstudioapi");
+# Call the Function
+inst_load(Required_Packages)
+
 # Changing working directory to source file location
-install.packages("rstudioapi")
-library(rstudioapi)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # This will load the bestfit program ccreated to fit distributions and a function to check for package requirements
-source("src/bestfit.R"); source("src/Install_And_Load.R")
-# The Packages needed to run the code 
-Required_Packages=c("openxlsx", "fBasics", "MASS", "data.table", "triangle", "shinydashboard", "shiny");
-# Call the Function
-Install_And_Load(Required_Packages)
+source("src/bestfit.R")
 
 #Loading the data into R (You have to change the filename to fit your own placement of the file)
 dat <- read.xlsx("Input-R.xlsx" ,sheet = 1, colNames = TRUE)
